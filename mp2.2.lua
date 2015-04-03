4c80b8d4d6704de6f64d6539c13f8c4208c21470
AeroHand <ketian.zhang@163.com>
2015-04-03 04:46:27 -0500

    引用table司马

--- mp2.2.lua
+++ mp2.2.lua
@@ -1,3 +1,6 @@
+--lua不支持堆栈 准备用多维表模拟堆栈
+--或者每次回溯结束后写个擦屁股函数
+
 function getFile(file_name)
   local f = assert(io.open(file_name, 'r'))
   count=0
@@ -28,7 +31,16 @@ function alphabeta(curstate,player,alpha,beta,depth)
         end
 		for j=1,6 do
 			if curstate[i][j]==0 then
+        local tempm=move(curstate,i,j,1,2)
+        local tempt={}
+        for i=1,6 do
+          tempt[i]={}
+           for j=1,6 do
+                tempt[i][j]=curstate[i][j]
+            end
+        end
 				temp=alphabeta(move(curstate,i,j,1,2),2,alpha,beta,depth+1)
+        curstate=tempm
 				if temp>value then
 					value=temp
 			    end
@@ -51,7 +63,7 @@ function alphabeta(curstate,player,alpha,beta,depth)
   			break
         end
 		for j=1,6 do
-			if curstate[i][j]==0 then
+			if curstate[i][j]==0 then    
 				temp=alphabeta(move(curstate,i,j,2,1),1,alpha,beta,depth+1)
 				if temp<value then
 					value=temp
@@ -71,6 +83,9 @@ function alphabeta(curstate,player,alpha,beta,depth)
 end
 
 function minmax(curstate,player,depth)
+  print("this try:")
+  deepprint(curstate)
+  print("player:",player,"depth:",depth)
   if finished(curstate) or (depth>3) then
     return eval(curstate)
   end  	
@@ -79,10 +94,16 @@ function minmax(curstate,player,depth)
   	for i=1,6 do
 		for j=1,6 do
 			if curstate[i][j]==0 then
-				temp=minmax(move(curstate,i,j,1,2),2,depth+1)
+        local tempm=move(curstate,i,j,1,2)
+        local stepp=step
+				temp=minmax(tempm,2,depth+1)
+        --deepprint(tempm)
+        curstate=moveback(curstate,stepp)
+        --deepprint(curstate)
+        print("step",stepp)
 				if temp>value then
 					value=temp
-			    end		
+			  end		
 			end
 	    end
 	end
@@ -92,7 +113,13 @@ function minmax(curstate,player,depth)
     for i=1,6 do
 		for j=1,6 do
 			if curstate[i][j]==0 then
-				temp=minmax(move(curstate,i,j,2,1),1,depth+1)
+        local tempm=move(curstate,i,j,2,1)
+				local stepp=step
+        temp=minmax(tempm,1,depth+1)
+        --deepprint(tempm)
+        curstate=moveback(curstate,stepp)
+        
+        print("step",stepp)
 				if temp<value then
 					value=temp
 			    end		
@@ -103,7 +130,36 @@ function minmax(curstate,player,depth)
   end  
 end
 
+function deepprint(yoyoyo)
+   for i=1,6,1 do
+    local temp=""
+    for j=1,6,1 do
+      temp=temp.." "..tostring(yoyoyo[i][j])
+    end  
+    print(temp)
+  end   
+  print("")
+end
+
+function moveback(curstate,step)
+       
+   for i=1,6 do
+    for j=1,6 do
+       curstate[i][j]=as[step][i][j]
+    end
+  end
+  return curstate  
+end  
+
 function move(curstate,i,j,player,nextplayer)
+  step=step+1
+  as[step]={}
+  for i=1,6 do
+    as[step][i]={}
+    for j=1,6 do
+      as[step][i][j]=curstate[i][j]
+    end
+  end    
 	curstate[i][j]=player
 	downx=i
 	upx=i
@@ -122,32 +178,37 @@ function move(curstate,i,j,player,nextplayer)
         upy=j+1
     end
     isdrop=true
-    for x=downx,upx do
-      for y=downy,upy do
-        if (curstate[x][y]==player) and (not(x==i) or not(y==j)) then
+      for x=downx,upx do
+
+        if (curstate[x][j]==player) and (not(x==i))  then
           isdrop=false
         end
+
       end
-    end
+
+      for y=downy,upy do
+        if (curstate[i][y]==player) and (not(y==j))  then
+          isdrop=false
+        end
+      end  
     --get ur enemy's land
     if not(isdrop) then
       for x=downx,upx do
-        for y=downy,upy do
-          if curstate[x][y]==nextplayer then
-            curstate[x][y]=player
-          end
+
+        if curstate[x][j]==nextplayer then
+          curstate[x][j]=player
         end
+
       end
+
+      for y=downy,upy do
+        if curstate[i][y]==nextplayer then
+          curstate[i][y]=player
+        end
+      end    
     end
-     for i=1,6,1 do
-  	local temp=""
-  	for j=1,6,1 do
-      temp=temp.." "..tostring(zou[i][j])
-    end  
-    print(temp)
-  end   
-  print("")
-    return curstate          
+
+  return curstate          
 end             	
 function finished(jjj)
 	local temp=true
@@ -176,21 +237,15 @@ end
 getFile("Narvik.txt")
 --state record for evaluation
 zou={}
+as={}
 for i=1,6 do
 	zou[i]={}
 	for j=1,6 do
       zou[i][j]=0
     end
 end
-
+step=0
 bvalue=minmax(zou,1,1,0)
 --bvalue=alphabeta(zou,1,1,-10000,10000,0)
 print(bvalue)
 
-for i=1,6,1 do
-  	local temp=""
-  	for j=1,6,1 do
-      temp=temp.." "..tostring(zou[i][j])
-    end  
-    print(temp)
-  end  
---
+++ test.lua
@@ -0,0 +1,10 @@
+ 
+counter = 0
+ 
+function test()
+   print("Depth:", counter)
+   counter = counter + 1
+   test()
+end
+ 
+test()
